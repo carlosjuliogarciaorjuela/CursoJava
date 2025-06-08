c1f7f6da900982f82af7d67ef9e9f496e19d7de5
carlosjuliogarciaorjuela <cjgarcia655@soy.sena.edu.co>
2025-06-08 16:25:20 -0500

    change app.java linea 5

--- Project/src/App.java
+++ Project/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.err.println("Dev");
+        System.err.println("Dev carlos");
     }
 }
